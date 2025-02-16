5677d6a3ee8d072bc4253131834f07a2caa5bb31
K23CNT1-NguyenHoaiNam <siver1856@gmail.com>
2025-02-16 15:09:04 +0700

    day02

--- Day02/nhn-app/src/App.js
+++ Day02/nhn-app/src/App.js
@@ -5,17 +5,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Nguyễn Hoài Nam
-        </p>
+        <img src={logo} className="App-logo" alt="logo2" />
+        <h1 className="App-title">Infomation</h1>
+        <p>Họ và tên: Nguyễn Hoài Nam</p>
+        <p>Mã sinh viên:  2310900073</p>
+        <p>Ngày sinh: 14/12/2005</p>
+        <p>Điện thoại: 0983460347*</p>
+        <p>Tên lớp: K23CNT1</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://facebook.com"
           target="_blank"
-          rel="noopener noreferrer"
         >
-          Learn React
+          Facebook
         </a>
       </header>
     </div>
