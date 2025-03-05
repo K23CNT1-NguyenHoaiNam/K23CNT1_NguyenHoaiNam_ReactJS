ae63f9a67f94646f95527a3dda5b2ff055777707
K23CNT1-NguyenHoaiNam1 <monkeygreen20@gmail.com>
2025-03-05 22:55:17 +0700

    Day08

---
+++ Day08/nhn-lesson08/src/NhnApp.js
@@ -0,0 +1,12 @@
+
+import './App.css';
+
+function NhnApp() {
+  return (
+    <div>
+      <h1 className='text-center'>Nguyen Hoai Nam - K23CNT1 - Miniproject1</h1>
+    </div>
+  );
+}
+
+export default NhnApp;
